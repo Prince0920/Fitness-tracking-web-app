0e218c21d4fd68faa5b7e42b615102002b485d5e
Prince Agrawal <prince.agrawal@dotsquares.com>
2023-06-28 18:38:30 +0530

    loadder changes

--- client/src/components/utils/Loader .js
+++ client/src/components/utils/Loader .js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Grid } from 'react-loader-spinner';
 import './Loader.css';
 
 const Loader = () => {
   return (
-    <div className="loader-container">
-      <div className="loader"></div>
+    <div className='loader-container'>
+      <Grid
+        height='80'
+        width='80'
+        color='#4fa94d'
+        ariaLabel='grid-loading'
+        radius='12.5'
+        wrapperStyle={{}}
+        wrapperClass=''
+        visible={true}
+      />
     </div>
   );
 };
