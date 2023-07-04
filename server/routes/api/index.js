7af65d3130d1c97519858af60def9fe641dbe1a4
Prince Agrawal <prince.agrawal@dotsquares.com>
2023-07-04 11:38:25 +0530

    server: tts

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,12 +1,14 @@
-const router = require("express").Router();
-const userRoutes = require("./user-routes");
-const exerciseRoutes = require("./exercise-routes");
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const exerciseRoutes = require('./exercise-routes');
 const fitbitRoutes = require('./fitbit/fitbit-routes');
 const userListRoute = require('./admin/user-list-route');
+const ttsRoute = require('./tts/tts-route');
 
-router.use("/user", userRoutes);
-router.use("/exercise", exerciseRoutes);
-router.use("/fitbit", fitbitRoutes);
-router.use("/admin", userListRoute);
+router.use('/user', userRoutes);
+router.use('/exercise', exerciseRoutes);
+router.use('/fitbit', fitbitRoutes);
+router.use('/admin', userListRoute);
+router.use('/tts', ttsRoute);
 
 module.exports = router;
---
+++ server/routes/api/tts/tts-route.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const {
+  speachText,
+  getAvailableLanguages,
+  getAvailableVoiceTypes,
+  getAvailableVoiceName,
+  saveAilableVoiceCustomeName,
+} = require('../../../controllers/tts/tts-controller');
+
+/* Google text to spech route start */
+// /api/tts/tts
+router.post('/tts', speachText);
+// /api/tts/get-available-languages
+router.get('/get-available-languages', getAvailableLanguages);
+// /api/tts/get-available-voice-types
+router.get('/get-available-voice-types', getAvailableVoiceTypes);
+// /api/tts/get-available-voice-name
+router.get('/get-available-voice-name', getAvailableVoiceName);
+// /api/tts/save-available-voice-custome-name
+router.post('/save-available-voice-custome-name', saveAilableVoiceCustomeName);
+// /api/tts/save-audio-device-drofile
+router.post('/save-audio-device-drofile', saveAilableVoiceCustomeName);
+// /api/tts/get-available-audio-device
+router.get('/get-available-audio-device', saveAilableVoiceCustomeName);
+/* Google text to spech route end */
+
+module.exports = router;
