2b09d55e6b2f1fee7274c675722b48c4d40b419c
Prince Agrawal <prince.agrawal@dotsquares.com>
2023-07-07 14:07:58 +0530

    loading managment

--- client/src/components/pages/app/fitbit/cards/LifetimeStatisticsCard/LifetimeStatisticsCard.js
+++ client/src/components/pages/app/fitbit/cards/LifetimeStatisticsCard/LifetimeStatisticsCard.js
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { getLifetimeStatics } from '../../../../../api/API';
 import GraphTitle from '../../../../../reusable/title/GraphTitle';
+import Loader from '../../../../../reusable/loader/Loader ';
 
 const { Title } = Typography;
 
 const LifetimeStatisticsCard = () => {
   const [total, setTotal] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const caloriesBurned = total?.caloriesOut;
   const totalSteps = total?.steps;
@@ -19,12 +21,13 @@ const LifetimeStatisticsCard = () => {
       try {
         const resp = await getLifetimeStatics(localStorage.getItem('token'));
         console.log('getLifetimeStatics', resp.data.lifetime.total);
+        setIsLoading(false);
         setTotal(resp.data.lifetime.total);
       } catch (error) {
         error.status === 400
           ? toast.info(error.data.message)
           : console.log('getLifetimeStatics error', error);
-
+        setIsLoading(true);
         // On error set default value
         setTotal({
           caloriesOut: 170000,
@@ -44,90 +47,94 @@ const LifetimeStatisticsCard = () => {
       <Card className='card card-primary card-outline'>
         <div className='card-body'>
           <div className='card-text'>
-            <Row
-              gutter={[16, 16]}
-              justify='space-between'>
-              <Col
-                xs={24}
-                sm={12}
-                md={6}>
-                <div className='progress-item'>
-                  <Title
-                    level={5}
-                    style={{ fontWeight: 'bold' }}>
-                    Calories Burned
-                  </Title>
-                  <Progress
-                    type='circle'
-                    percent={100}
-                    strokeColor='#FF6384'
-                    trailColor='#D3D3D3'
-                    strokeLinecap='square'
-                    format={() => `${caloriesBurned}\nkcal`}
-                  />
-                </div>
-              </Col>
-              <Col
-                xs={24}
-                sm={12}
-                md={6}>
-                <div className='progress-item'>
-                  <Title
-                    level={5}
-                    style={{ fontWeight: 'bold' }}>
-                    Total Steps
-                  </Title>
-                  <Progress
-                    type='circle'
-                    percent={100}
-                    strokeColor='#36A2EB'
-                    trailColor='#D3D3D3'
-                    strokeLinecap='square'
-                    format={() => `${totalSteps}\nsteps`}
-                  />
-                </div>
-              </Col>
-              <Col
-                xs={24}
-                sm={12}
-                md={6}>
-                <div className='progress-item'>
-                  <Title
-                    level={5}
-                    style={{ fontWeight: 'bold' }}>
-                    Distance Traveled
-                  </Title>
-                  <Progress
-                    type='circle'
-                    percent={100}
-                    strokeColor='#FFCE56'
-                    trailColor='#D3D3D3'
-                    strokeLinecap='square'
-                    format={() => `${distanceTraveled}\nkm`}
-                  />
-                </div>
-              </Col>
-              <Col
-                xs={24}
-                sm={12}
-                md={6}>
-                <div className='progress-item'>
-                  <Title
-                    level={5}
-                    style={{ fontWeight: 'bold' }}>
-                    Active Score
-                  </Title>
-                  <Progress
-                    type='circle'
-                    percent={100}
-                    strokeColor='#00C851'
-                    trailColor='#D3D3D3'
-                    strokeLinecap='square'
-                    format={() => `${activeScore}`}
-                  />
-                </div>
-              </Col>
-            </Row>
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <Row
+                gutter={[16, 16]}
+                justify='space-between'>
+                <Col
+                  xs={24}
+                  sm={12}
+                  md={6}>
+                  <div className='progress-item'>
+                    <Title
+                      level={5}
+                      style={{ fontWeight: 'bold' }}>
+                      Calories Burned
+                    </Title>
+                    <Progress
+                      type='circle'
+                      percent={100}
+                      strokeColor='#FF6384'
+                      trailColor='#D3D3D3'
+                      strokeLinecap='square'
+                      format={() => `${caloriesBurned}\nkcal`}
+                    />
+                  </div>
+                </Col>
+                <Col
+                  xs={24}
+                  sm={12}
+                  md={6}>
+                  <div className='progress-item'>
+                    <Title
+                      level={5}
+                      style={{ fontWeight: 'bold' }}>
+                      Total Steps
+                    </Title>
+                    <Progress
+                      type='circle'
+                      percent={100}
+                      strokeColor='#36A2EB'
+                      trailColor='#D3D3D3'
+                      strokeLinecap='square'
+                      format={() => `${totalSteps}\nsteps`}
+                    />
+                  </div>
+                </Col>
+                <Col
+                  xs={24}
+                  sm={12}
+                  md={6}>
+                  <div className='progress-item'>
+                    <Title
+                      level={5}
+                      style={{ fontWeight: 'bold' }}>
+                      Distance Traveled
+                    </Title>
+                    <Progress
+                      type='circle'
+                      percent={100}
+                      strokeColor='#FFCE56'
+                      trailColor='#D3D3D3'
+                      strokeLinecap='square'
+                      format={() => `${distanceTraveled}\nkm`}
+                    />
+                  </div>
+                </Col>
+                <Col
+                  xs={24}
+                  sm={12}
+                  md={6}>
+                  <div className='progress-item'>
+                    <Title
+                      level={5}
+                      style={{ fontWeight: 'bold' }}>
+                      Active Score
+                    </Title>
+                    <Progress
+                      type='circle'
+                      percent={100}
+                      strokeColor='#00C851'
+                      trailColor='#D3D3D3'
+                      strokeLinecap='square'
+                      format={() => `${activeScore}`}
+                    />
+                  </div>
+                </Col>
+              </Row>
+            )}
           </div>
         </div>
       </Card>
