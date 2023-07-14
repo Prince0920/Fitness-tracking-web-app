9b48c34933cc7957d47276ac152bac21fcce557b
Prince Agrawal <prince.agrawal@dotsquares.com>
2023-07-14 16:28:21 +0530

    login register api for apple admin

--- server/controllers/apple/admin/apple-admin-controller.js
+++ server/controllers/apple/admin/apple-admin-controller.js
@@ -3,11 +3,34 @@ const mongoose = require('mongoose');
 const axios = require('axios');
 
 module.exports = {
-  // Testing fitbit routing
+  // Admin apple login
+  loginAppleWatch(req, res) {
+    try {
+      console.log('loginAppleWatch body data', req.body);
+      console.log('loginAppleWatch query data', req.query);
+      res.json({ message: 'loginAppleWatch' });
+    } catch (error) {
+      console.log('error in loginAppleWatch', error);
+      return res.status(500).json({ message: 'Something went wrong!' });
+    }
+  },
+
+  // Apple admin signup
+  registerAppleWatch(req, res) {
+    try {
+      console.log('registerAppleWatch body data', req.body);
+      console.log('registerAppleWatch query data', req.query);
+      res.json({ message: 'registerAppleWatch' });
+    } catch (error) {
+      console.log('error in registerAppleWatch apple', error);
+      return res.status(500).json({ message: 'Something went wrong!' });
+    }
+  },
+  // Testing apple routing
   activityData(req, res) {
     try {
-      console.log('activity body data', req.body)
-      res.json({ message: 'activityData: '+  req.body.activityData});
+      console.log('activity body data', req.body);
+      res.json({ message: 'activityData: ' + req.body.activityData });
     } catch (error) {
       console.log('error in activityData apple', error);
       return res.status(500).json({ message: 'Something went wrong!' });
--- server/routes/api/apple/admin/apple-admin-routes.js
+++ server/routes/api/apple/admin/apple-admin-routes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
 const passport = require('passport');
-const { activityData } = require('../../../../controllers/apple/admin/apple-admin-controller');
+const { activityData, loginAppleWatch, registerAppleWatch } = require('../../../../controllers/apple/admin/apple-admin-controller');
+
+// /api/apple/admin/login
+router.route('/login').get(loginAppleWatch);
+
+// /api/apple/admin/register
+router.route('/register').post(registerAppleWatch);
 
 // /api/apple/admin/activity-data
 router.route('/activity-data').post(activityData);
