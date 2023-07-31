93137f8e91fc6120547dcd2beb6abbefc590b729
Prince Agrawal <prince.agrawal@dotsquares.com>
2023-07-31 12:30:08 +0530

    nodejs cron job setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const cron = require("node-cron");
 const routes = require('./routes');
 const db = require('./config/connection');
 const session = require('express-session');
@@ -23,10 +24,18 @@ app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: fals
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Creating a cron job which runs on every 10 second
+// cron.schedule("*/10 * * * * *", function() {
+//   console.log("running a task every 10 second");
+// });
+
+
 app.use(routes);
 
+const ip = 'localhost'
+// 192.168.6.76
 db.once('open', () => {
-  app.listen(PORT, '192.168.6.76',() => {
-    console.log(`API server running on port ${PORT}!`);
+  app.listen(PORT, ip,() => {
+    console.log(`API server running on port http://${ip}:${PORT}!`);
   });
 });
